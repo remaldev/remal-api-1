f21f1fd6a3ceeccc438bda661d7469c126fd449e
aallali <contact@allali.me>
2025-02-23 17:40:52 +0000

    [FEAT](api) Add `ValidationPipe` globally

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable global validation pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // NOTE: if forbidNonWhitelisted is set to true, then the whitelist is useless
+      whitelist: true, // Automatically strips properties that do not have any decorators
+      forbidNonWhitelisted: true, // Rejects request if there are any properties not part of the DTO
+      transform: true, // Automatically transforms payloads to DTOs
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
