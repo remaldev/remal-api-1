c54b57c21906e07f93841ce1a5425addb9a1331e
aallali <contact@allali.me>
2025-02-23 17:43:26 +0000

    [FEAT](user) Add `createUser` controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post()
+  createUser(@Body() userCreateDto: CreateUserDto) {
+    return this.userService.createUser(userCreateDto);
+  }
 
   @Get()
   getHello() {
