18e9d7f017d19c655244da0ef980c6c0664e689c
aallali <contact@allali.me>
2025-03-02 00:54:24 +0000

    [TEST] Refactor `Jest` configs for `paths`+`coverage`+`global-setup`

--- test/jest.config.ts
+++ test/jest.config.ts
@@ -5,49 +5,51 @@ const buildProject = (
   folderName: string,
 ): JestConfigTypes.InitialProjectOptions => ({
   displayName: folderName,
-  ...(folderName === 'e2e' && {
-    globalSetup: '<rootDir>/test/jest-global-setup.ts',
-    globalTeardown: '<rootDir>/test/jest-global-teardown.ts',
-  }),
   rootDir: '.',
   testMatch: [
     folderName === 'e2e'
-      ? '<rootDir>/test/**/*.e2e-spec.ts'
+      ? '<rootDir>/test/e2e/**/*.e2e-spec.ts'
       : '<rootDir>/src/**/*.spec.ts',
   ],
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
-  moduleNameMapper: {
-    '^src/(.*)$': '<rootDir>/src/$1',
-  },
+  setupFilesAfterEnv: ['<rootDir>/test/jest-setup.ts'],
 })
 
 const jestConfig: JestConfig = {
-  rootDir: '.',
+  rootDir: '../',
   clearMocks: true,
-  collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main.ts',
     '!<rootDir>/src/**/*.mock.ts',
     '!<rootDir>/src/**/*.module.ts',
     '!<rootDir>/src/**/*.spec.ts',
+    '!<rootDir>/src/**/*.e2e-spec.ts',
     '!<rootDir>/src/**/*.test.ts',
     '!<rootDir>/src/**/*.d.ts',
   ],
-  coverageReporters: ['text', 'json', 'cobertura', 'lcov', 'html'],
-  coverageDirectory: '../coverage',
+  coverageReporters: [
+    'text',
+    'json',
+    'cobertura',
+    'lcov',
+    'html',
+    'text-summary',
+  ],
+  coverageDirectory: './coverage',
   modulePathIgnorePatterns: [
     '<rootDir>/dist/',
     '<rootDir>/lib/',
     '<rootDir>/tmp/',
   ],
+  globalSetup: '<rootDir>/test/jest-global-setup.ts',
+  globalTeardown: '<rootDir>/test/jest-global-teardown.ts',
   projects: [buildProject('unit'), buildProject('e2e')],
   silent: true,
   testEnvironment: 'node',
   verbose: false,
-  moduleNameMapper: {
-    '^src/(.*)$': '<rootDir>/src/$1',
-  },
 }
+
 export default jestConfig
