5f990a0d9a170b2f41b3bdbbf22943b69d178af3
aallali <contact@allali.me>
2025-03-12 14:17:05 +0000

    [FEAT](api) Create `bootstrapApp` function for reusability

---
+++ src/app.bootstrap.ts
@@ -0,0 +1,37 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { GlobalExceptionFilter } from './common/filters/http-exception.filter'
+import * as packageJson from '../package.json'
+
+/**
+ * Apply common application configurations
+ * This function is used by both main.ts and test setup
+ */
+export function bootstrapApp(app: INestApplication): void {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  )
+
+  app.useGlobalFilters(new GlobalExceptionFilter())
+
+  const config = new DocumentBuilder()
+    .setTitle(packageJson.name)
+    .setDescription(packageJson.description)
+    .setVersion(packageJson.version)
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api-docs', app, document, {
+    swaggerOptions: {
+      filter: true,
+      showRequestDuration: true,
+      persistAuthorization: true,
+    },
+  })
+
+  app.useGlobalFilters(new GlobalExceptionFilter())
+}
--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,23 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { LogLevel, ValidationPipe } from '@nestjs/common'
+import { bootstrapApp } from './app.bootstrap'
 import { ConfigService } from '@nestjs/config'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
-import * as packageJson from '../package.json'
+import { LogLevel } from '@nestjs/common'
 
-async function bootstrap() {
+async function init() {
   const app = await NestFactory.create(AppModule)
+
+  bootstrapApp(app)
+
+  // Additional main.ts specific configurations
+  // (if any - like port configuration, CORS, etc.)\
   const configService = app.get(ConfigService)
-
-  // Enable global validation pipe
-  app.useGlobalPipes(
-    new ValidationPipe({
-      // NOTE: if forbidNonWhitelisted is set to true, then the whitelist is useless
-      whitelist: true, // Automatically strips properties that do not have any decorators
-      forbidNonWhitelisted: true, // Rejects request if there are any properties not part of the DTO
-      transform: true, // Automatically transforms payloads to DTOs
-    }),
-  )
-
-  const config = new DocumentBuilder()
-    .setTitle(packageJson.name)
-    .setDescription(packageJson.description)
-    .setVersion(packageJson.version)
-    .addBearerAuth()
-    .build()
-
-  const document = SwaggerModule.createDocument(app, config)
-
-  SwaggerModule.setup('api-docs', app, document, {
-    swaggerOptions: {
-      filter: true,
-      showRequestDuration: true,
-      persistAuthorization: true,
-    },
-  })
-
   const logLevel = configService.get<string>('LOG_LEVEL')
   const port = configService.get<number>('PORT', 3000)
 
   app.useLogger([logLevel as LogLevel])
+
   await app.listen(port)
 }
 
-bootstrap()
+init()
