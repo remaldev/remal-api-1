66f5ec6baaeac2b222079d3637a7dd9273d40b8e
aallali <contact@allali.me>
2025-04-11 00:50:30 +0100

    [REFACT] Update configuration for new env loader
    no need to specify env prefix
    dotenv cli will load correct file

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,16 +1,8 @@
-const getEnvVar = (name: string, nodeEnv: string): string | undefined => {
-  const env = process.env
-  const prefixedVar = env[`${nodeEnv}_${name}`]
-  return prefixedVar || env[name]
-}
-
 export default () => {
-  const nodeEnv = process.env.NODE_ENV || 'dev'
-
   return {
-    DB_POSTGRE_URI: getEnvVar('DB_POSTGRE_URI', nodeEnv),
-    DB_MONGO_URI: getEnvVar('DB_MONGO_URI', nodeEnv),
-    LOG_LEVEL: getEnvVar('LOG_LEVEL', nodeEnv),
-    PORT: getEnvVar('PORT', nodeEnv),
+    DB_POSTGRE_URI: process.env.DB_POSTGRE_URI,
+    DB_MONGO_URI: process.env.DB_MONGO_URI,
+    LOG_LEVEL: process.env.LOG_LEVEL,
+    PORT: process.env.PORT,
   }
 }
