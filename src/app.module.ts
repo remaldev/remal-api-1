2c089ea884784252b1fe977ef29dae8f09a60302
aallali <contact@allali.me>
2025-09-11 23:09:32 +0100

    [FEAT](mailer) Create Mailer module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { PrismaModule } from './prisma/prisma.module'
-import { ConfigModule } from '@nestjs/config'
-import { UserModule } from './user/user.module';
 import configuration from './config/configuration'
+import { MailerModule } from './mailer/mailer.module'
+import { PrismaModule } from './prisma/prisma.module'
+import { UserModule } from './user/user.module'
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import configuration from './config/configuration'
     }),
     PrismaModule,
     UserModule,
+    MailerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
