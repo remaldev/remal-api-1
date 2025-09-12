83e3c9e1ec0896c55ef2d69ef8642c94e9ece1b6
aallali <contact@allali.me>
2025-09-12 12:39:37 +0100

    [FEAT](auth) Create `CurrentUser` decorator

--- src/auth/decorators.ts
+++ src/auth/decorators.ts
@@ -1,8 +1,22 @@
-import { SetMetadata } from '@nestjs/common'
+import {
+  createParamDecorator,
+  ExecutionContext,
+  SetMetadata,
+} from '@nestjs/common'
 import { Role } from './enums'
+import { AuthenticatedUser } from './interfaces'
 
 export const IS_PUBLIC_KEY = 'isPublic'
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true)
 
 export const ROLES_KEY = 'roles'
 export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles)
+
+export const CurrentUser = createParamDecorator(
+  (data: string, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest()
+    const user = request.user
+
+    return (data ? user?.[data] : user) as AuthenticatedUser
+  },
+)
