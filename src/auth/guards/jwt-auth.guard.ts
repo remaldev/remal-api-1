8100819f53fff2e537d27886cba9cef5916f013a
aallali <contact@allali.me>
2025-09-12 12:30:01 +0100

    [FEAT](auth) Create JwtAuthGuard

---
+++ src/auth/guards/jwt-auth.guard.ts
@@ -0,0 +1,27 @@
+import { ExecutionContext, Injectable } from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
+import { AuthGuard } from '@nestjs/passport'
+import { IS_PUBLIC_KEY } from '../decorators'
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  private readonly reflector: Reflector
+
+  constructor(reflector: Reflector) {
+    super()
+    this.reflector = reflector
+  }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ])
+
+    if (isPublic) {
+      return true
+    }
+
+    return super.canActivate(context)
+  }
+}
