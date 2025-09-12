75335dcc0d22ec3c3fd705733d6926531fce1e1c
aallali <contact@allali.me>
2025-09-12 12:42:47 +0100

    [FEAT](auth) Register `AuthModule` in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { APP_GUARD } from '@nestjs/core'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { AuthModule } from './auth/auth.module'
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard'
 import configuration from './config/configuration'
 import { MailerModule } from './mailer/mailer.module'
 import { PrismaModule } from './prisma/prisma.module'
@@ -16,8 +19,18 @@ import { UserModule } from './user/user.module'
     PrismaModule,
     UserModule,
     MailerModule,
+    AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      // Register JwtAuthGuard as a global guard (applies to all routes by default)
+      // This means the guard runs automatically on every route unless you explicitly
+      // mark a route as @Public() (if you’ve set up a decorator to bypass the guard).
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
