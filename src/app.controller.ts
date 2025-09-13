6ff34b4e1a1258d96a57d8529e33f34db91c66ae
aallali <contact@allali.me>
2025-09-13 23:14:30 +0100

    refact(app): set `getHello` controller as `Public`

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
+import { Public } from './auth/decorators'
 
 @Controller()
 export class AppController {
@@ -9,6 +10,7 @@ export class AppController {
     this.appService = appService
   }
 
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello()
