dff9960022283afda2f530f01316e83d72382b94
aallali <contact@allali.me>
2025-09-20 19:48:56 +0100

    style: re order imports and code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import { bootstrapApp } from './app.bootstrap'
-import { ConfigService } from '@nestjs/config'
 import { LogLevel } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { NestFactory } from '@nestjs/core'
+import { bootstrapApp } from './app.bootstrap'
+import { AppModule } from './app.module'
 
 async function init() {
   const app = await NestFactory.create(AppModule)
