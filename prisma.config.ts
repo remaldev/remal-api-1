841e3bc6e11d8bdaf2111814266b98bba63dbe30
aallali <hi@allali.me>
2025-12-11 22:53:28 +0100

    refact(prisma): reconfig prisma setup for new version

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,14 @@
-import 'dotenv/config'
 import path from 'node:path'
 import { defineConfig } from 'prisma/config'
+import configuration from './src/config/configuration'
 
 export default defineConfig({
   schema: path.join('prisma', 'schema.prisma'),
   migrations: {
     path: path.join('prisma', 'migrations'),
     seed: 'node prisma/prisma.seed.ts',
+  },
+  datasource: {
+    url: configuration().DB_POSTGRE_URI as string,
   },
 })
