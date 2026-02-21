75582415ed13f771d16897db1a6057addd5ef75c
aallali <hi@allali.me>
2026-02-21 23:45:04 +0000

    feat(shop): add `createShop` Service

---
+++ src/pos/shop/shop.service.ts
@@ -0,0 +1,34 @@
+import { ConflictException, Injectable } from '@nestjs/common'
+import { PrismaService } from '../../prisma/prisma.service'
+import { CreateShopDto, ShopResponseDto, toShopResponseDto } from './shop.dto'
+
+@Injectable()
+export class ShopService {
+  private readonly prismaService: PrismaService
+
+  constructor(prismaService: PrismaService) {
+    this.prismaService = prismaService
+  }
+
+  async createShop(
+    createShopDto: CreateShopDto,
+    ownerId: string,
+  ): Promise<ShopResponseDto> {
+    const total_shops_by_owner = await this.prismaService.shop.count({
+      where: { ownerId },
+    })
+    if (total_shops_by_owner >= 1) {
+      //  throw status code for limit reached (409 Conflict)
+      throw new ConflictException(
+        'Shop creation limit reached for this owner (1 shop max).',
+      )
+    }
+    const shop = await this.prismaService.shop.create({
+      data: {
+        name: createShopDto.name,
+        ownerId: ownerId,
+      },
+    })
+    return toShopResponseDto(shop)
+  }
+}
