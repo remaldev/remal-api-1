09449e8b3f4e47587e95b7cb27a9f47acc9e7110
aallali <hi@allali.me>
2026-02-22 01:52:48 +0000

    feat(pos): Register `ProductModule` in POS module

--- src/pos/pos.module.ts
+++ src/pos/pos.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common'
 import { RouterModule } from '@nestjs/core'
+import { ProductModule } from './product/product.module'
 import { ShopModule } from './shop/shop.module'
 
 @Module({
   imports: [
     ShopModule,
+    ProductModule,
     RouterModule.register([
       {
         path: 'pos',
         module: ShopModule,
+      },
+      {
+        path: 'pos',
+        module: ProductModule,
       },
     ]),
   ],
